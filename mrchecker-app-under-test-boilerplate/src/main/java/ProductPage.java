74d5c1164b6bc08a77da2fc30266324bac171614
anetakmiecik <aneta.kmiecik@capgemini.com>
2018-11-17 21:43:33 +0100

    addedProductPage

---
+++ mrchecker-app-under-test-boilerplate/src/main/java/ProductPage.java
@@ -0,0 +1,23 @@
+import com.capgemini.mrchecker.selenium.core.BasePage;
+
+public class ProductPage extends BasePage {
+	
+	@Override
+	public boolean isLoaded() {
+		// TASK Auto-generated method stub
+		return false;
+	}
+	
+	@Override
+	public void load() {
+		// TASK Auto-generated method stub
+		
+	}
+	
+	@Override
+	public String pageTitle() {
+		// TASK Auto-generated method stub
+		return null;
+	}
+	
+}
