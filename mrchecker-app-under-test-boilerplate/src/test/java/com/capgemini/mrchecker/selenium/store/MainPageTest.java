7f3d5fafd7b301c6ffa95445c6aae2078be00348
anetakmiecik <aneta.kmiecik@capgemini.com>
2018-11-20 10:45:12 +0100

    parametrization

--- mrchecker-app-under-test-boilerplate/src/test/java/com/capgemini/mrchecker/selenium/store/MainPageTest.java
+++ mrchecker-app-under-test-boilerplate/src/test/java/com/capgemini/mrchecker/selenium/store/MainPageTest.java
@@ -7,14 +7,16 @@ import java.util.List;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized.Parameters;
 
 import com.capgemini.mrchecker.test.core.BaseTest;
 import com.capgemini.mrchecker.test.core.logger.BFLogger;
 
+import junitparams.JUnitParamsRunner;
+
+@RunWith(JUnitParamsRunner.class)
 public class MainPageTest extends BaseTest {
-	
-	public static String	PRODUCT_1	= "Apple iPod touch Large";
-	public static String	PRODUCT_2	= "Apple iPad 2 16GB, Wi-Fi, 9.7in - Black";
 	
 	private static MainPage storeMainPage;
 	
@@ -43,25 +45,26 @@ public class MainPageTest extends BaseTest {
 	}
 	
 	@Test
-	public void shouldAddProductsToCart() {
-		// Add first product to cart and submit search
-		ResultsPage resultsPage = storeMainPage.searchProduct(PRODUCT_1);
+	@Parameters(name = "testData")
+	public void shouldAddProductsToCart(String productName) {
+		
+		BFLogger.logInfo("Search for product");
+		ResultsPage resultsPage = storeMainPage.searchProduct(productName);
 		resultsPage.clickAddToCart();
 		resultsPage.clickReturnHomeButton();
 		
-		// Add second product to cart and submit search
-		resultsPage = storeMainPage.searchProduct(PRODUCT_2);
-		resultsPage.clickAddToCart();
-		resultsPage.clickReturnHomeButton();
-		
+		BFLogger.logInfo("Open cart");
 		CartPage cartPage = storeMainPage.clickCartButton();
 		
+		BFLogger.logInfo("Get product list");
 		List<String> productListFromCart = cartPage.getProductList();
 		
-		System.out.println(productListFromCart);
-		
-		assertTrue(PRODUCT_1 + " is not on list", productListFromCart.contains(PRODUCT_1));
-		assertTrue(PRODUCT_2 + " is not on list", productListFromCart.contains(PRODUCT_2));
+		BFLogger.logInfo("Assert the searched product is in the cart");
+		assertTrue(productName + " is not on list", productListFromCart.contains(productName));
+	}
+	
+	private Object[] testData() {
+		return new Object[] { "Apple iPod touch Large", "Apple iPad 2 16GB, Wi-Fi, 9.7in - Black" };
 	}
 	
 }
